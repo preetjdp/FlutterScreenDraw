b23af3177027fd96d28de7696815d2d73e0aad5f
Preet Parekh <preetjdp123@gmail.com>
2018-12-08 22:15:51 +0530

    Changed NavBar Color

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp( MaterialApp(
-  home: HomePage(),
-));
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+  ));
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      systemNavigationBarColor: Colors.white,
+      systemNavigationBarIconBrightness: Brightness.dark));
+}
 
 class HomePage extends StatefulWidget {
   @override
@@ -10,9 +16,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
-  List<Offset> _points = <Offset> [];
-
+  List<Offset> _points = <Offset>[];
 
   @override
   Widget build(BuildContext context) {
@@ -21,10 +25,11 @@ class _HomePageState extends State<HomePage> {
         child: GestureDetector(
           onPanUpdate: (DragUpdateDetails details) {
             setState(() {
-                          RenderBox object = context.findRenderObject();
-                          Offset _localPosition = object.globalToLocal(details.globalPosition);
-                          _points = List.from(_points)..add(_localPosition);
-                        });
+              RenderBox object = context.findRenderObject();
+              Offset _localPosition =
+                  object.globalToLocal(details.globalPosition);
+              _points = List.from(_points)..add(_localPosition);
+            });
           },
           onPanEnd: (DragEndDetails details) => _points.add(null),
           child: CustomPaint(
@@ -44,31 +49,26 @@ class _HomePageState extends State<HomePage> {
 }
 
 class Signature extends CustomPainter {
-
   List<Offset> points;
 
   Signature({this.points});
 
   @override
   void paint(Canvas canvas, Size size) {
-
     Paint paint = Paint()
-    ..color = Colors.black
-    ..strokeCap = StrokeCap.round
-    ..strokeWidth = 5.0;
-    
+      ..color = Colors.black
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 5.0;
 
-    for(int i = 0; i < points.length - 1 ; i++ ) {
-      if(points[i] != null && points[i+1] != null) {
-        canvas.drawLine(points[i], points[i+1], paint);
+    for (int i = 0; i < points.length - 1; i++) {
+      if (points[i] != null && points[i + 1] != null) {
+        canvas.drawLine(points[i], points[i + 1], paint);
       }
     }
-
   }
 
   @override
   bool shouldRepaint(Signature oldDelegate) {
     return oldDelegate.points != points;
   }
-
 }
